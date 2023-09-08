fe5cf9887521b4644d9acb64699e31a673ff02c5
Reece Cosbey <rcosbey@ut.utm.edu>
2023-09-08 12:02:10 -0500

    Adding the second file of the project

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 #include <iostream>
 
 using namespace std;
-
+int sum(int n){
+  int i = 1;
+  int n2 = n;
+  while (i != n){
+    n2 = n2 + i;
+    i++;
+  }
+  return n2;
+}
 int main() {
-  cout << "Hello World!";
-  
+  int number;
+  cout << "Input a number ";
+  cin >> number;
+  number = sum(number);
+  cout << number << endl;
   return 0;
 }
